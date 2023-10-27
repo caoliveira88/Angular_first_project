20974bc4edeec59f79d471e4cd371de1d9cdaf14
caoliveira88 <camilaoliver1@gmail.com>
2023-10-27 16:35:11 -0300

    :sparkles: feat - footer component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
-import {MatToolbarModule } from '@angular/material/toolbar'; // used material lib
+import {MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component'; // used material lib
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
